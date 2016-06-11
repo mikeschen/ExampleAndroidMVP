7fd8b447998dc0b1c54d357404572dc76b11c3b3
Michael Schennum <mikeschenphoto@gmail.com>
2016-06-11 15:49:51 -0700

    new mvp

--- app/src/main/java/com/mikeschen/www/todoandroidmvp/MainActivity.java
+++ app/src/main/java/com/mikeschen/www/todoandroidmvp/MainActivity.java
@@ -1,13 +1,10 @@
 package com.mikeschen.www.todoandroidmvp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends AppCompatActivity{
 }
