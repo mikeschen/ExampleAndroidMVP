9811be038e305e69c8778b3e2d199fa2f3571b57
Michael Schennum <mikeschenphoto@gmail.com>
2016-06-11 16:17:27 -0700

    fixes setText

--- app/src/main/java/com/mikeschen/www/todoandroidmvp/Model/User.java
+++ app/src/main/java/com/mikeschen/www/todoandroidmvp/Model/User.java
@@ -22,6 +22,6 @@ public class User {
 
     @Override
     public String toString() {
-        return "FullName : " + fullName + "/nEmail : " + email;
+        return "Full Name : " + fullName + " Email : " + email;
     }
 }
